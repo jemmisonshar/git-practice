2a28cfdf5e35377b84fec6ac299fa5af11381c12
jemmisonshar <jemmisonshar@gmail.com>
2025-03-12 09:39:47 +0000

    Add 7.js

---
+++ src/7.js
@@ -0,0 +1,11 @@
+function getRandomNodeCode() {
+  const code = `const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});`;
+return code;
+}
