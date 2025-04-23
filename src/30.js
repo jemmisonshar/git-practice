3e7ab0d9bcff20ba6676af80d96163d653802229
jemmisonshar <jemmisonshar@gmail.com>
2025-04-23 10:32:15 +0000

    Add 30.js

---
+++ src/30.js
@@ -0,0 +1,15 @@
+function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // Output: true
